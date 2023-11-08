ea8a06f4278715d82de0481dd3aaba529c63f870
AdityaTiwari03 <aadi.tiwari0208@gmail.com>
2023-11-08 07:45:09 +0530

    Responsive

--- src/Comp/Info.js
+++ src/Comp/Info.js
@@ -1,29 +1,30 @@
-import React, {useState, useToggle} from "react";
+import React from "react";
 import "react-awesome-button/dist/styles.css";
 import pdf from "./Resume_Aditya.pdf";
-import  "./info.css"
+import "./info.css";
 export default function Info() {
-  function Open(){
-  }
-   return (
+  return (
     <div className="portfolio-container">
       <div className="info-container">
         <div className="basic-info">
-        <h1 className="name">Aditya Tiwari</h1>
-        <h3 className="info">
-          IIT Bhilai, Btech (2021-2025) <br />
-          Computer Science and Engineering
-        </h3>
-        <div className="button-container">
-        <a href={pdf} without rel="noopener noreferrer"  target="_blank">
-      <button trailingIcon="picture_as_pdf"  className="pdf" label="Resume">
-        View Portfolio
-      </button>
-   </a>        </div>
+          <h1 className="name">Aditya Tiwari</h1>
+          <h3 className="info">
+            IIT Bhilai, Btech (2021-2025) <br />
+            Computer Science and Engineering
+          </h3>
+          <div className="button-container">
+            <a href={pdf} without rel="noopener noreferrer" target="_blank">
+              <button
+                trailingIcon="picture_as_pdf"
+                className="pdf"
+                label="Resume"
+              >
+                View Portfolio
+              </button>
+            </a>{" "}
+          </div>
         </div>
-
       </div>
     </div>
-  
- );
+  );
 }
--- src/Comp/Tab.js
+++ src/Comp/Tab.js
@@ -1,64 +1,70 @@
-import React, {useState, useEffect} from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import "./tabcss.css";
 
 export default function UpTab() {
-  const [navbarHeight, setNavbarHeight] = useState('20px');
+  const [navbarHeight, setNavbarHeight] = useState("20px");
   window.onscroll = function (event) {
-    var scroll = window.scrollY
-    if (scroll < 500){
+    var scroll = window.scrollY;
+    if (scroll < 500) {
       document.getElementById("navbar").style.backgroundColor = "";
-    }   
-    else {
+    } else {
       document.getElementById("navbar").style.backgroundColor = "black";
     }
-  } 
-    const handleClick = () => {
-    setNavbarHeight((prevHeight) => (prevHeight === '30px' ? 'fit-content' : '30px'));
-    
+  };
+  const handleClick = () => {
+    setNavbarHeight((prevHeight) =>
+      prevHeight === "30px" ? "fit-content" : "30px"
+    );
   };
 
   return (
     <div className="main">
       <div className="container-fluid">
-        <ul className="navbar" id ="navbar">
-        <li className="navbar-item">
-          <Link activeClass="active" to="home" spy={true} smooth={true} onClick={handleClick}>
-            Overview
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="education" spy={true} smooth={true}>
-            Education
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="project" spy={true} smooth={true}>
-            Project
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="skill" spy={true} smooth={true}>
-            Skills
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="extra" spy={true} smooth={true}>
-            ExtraCurricular
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="Personal" spy={true} smooth={true}>
-            Personal Info
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="Contact" spy={true} smooth={true}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+        <ul className="navbar" id="navbar">
+          <li className="navbar-item">
+            <Link
+              activeClass="active"
+              to="home"
+              spy={true}
+              smooth={true}
+              onClick={handleClick}
+            >
+              Overview
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="education" spy={true} smooth={true}>
+              Education
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="project" spy={true} smooth={true}>
+              Project
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="skill" spy={true} smooth={true}>
+              Skills
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="extra" spy={true} smooth={true}>
+              ExtraCurricular
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="Personal" spy={true} smooth={true}>
+              Personal Info
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="Contact" spy={true} smooth={true}>
+              Contact
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
-};
+}
